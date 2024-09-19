911b0a8a974b7cf0659a5f7f2839fd7bdf3b297c
YskHmd <yusuke7075@gmail.com>
2024-09-19 14:33:44 +0900

    home追加

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc, goodfunc, readfunc, BoardCreate, deletefunc
 
 urlpatterns = [
+    path('', listfunc, name='home'),  # この行を追加
     path('signup/',signupfunc, name='signup'),
     path('login/',loginfunc, name='login'),
     path('list/', listfunc, name='list'),
