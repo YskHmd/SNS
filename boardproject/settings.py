c954ace4b6d27d929faf045fdb7c54ad196c49ea
EC2 Default User <ec2-user@ip-172-31-32-34.ap-northeast-3.compute.internal>
2024-09-29 04:05:59 +0000

    a

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z6vjl4$&i_ew_=fh72-y*$j@yfof0yjh_u7jpute)cc0m$@7gm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.208.192.208', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['13.208.192.208', 'localhost', '127.0.0.1', 'hamada-dev.com', 'www.hamada-dev.com']
 
 
 # Application definition
